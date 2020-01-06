e020343d8496d82d2bf070fd9a650b4ecfbb5758
franciscojg <francisco.goonzalez99@gmail.com>
2020-01-06 10:34:54 -0300

    h

---
+++ crud_pdo/add_modal.php
@@ -0,0 +1,58 @@
+<!-- Agregar Nuevo -->
+<div class="modal fade" id="addnew" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+            	<h4 class="modal-title" id="myModalLabel">Crear ticket</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true">&times;</span>
+		        </button>
+                
+            </div>
+            <div class="modal-body">
+			<div class="container-fluid">
+			<form method="POST" action="add.php">
+				<div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label" style="position:relative; top:7px;">Nombre:</label>
+					</div>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" name="firstname">
+					</div>
+				</div>
+				<div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label" style="position:relative; top:7px;">Apellido:</label>
+					</div>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" name="lastname">
+					</div>
+				</div>
+				<div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label" style="position:relative; top:7px;">Detalle:</label>
+					</div>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" name="address">
+					</div>
+				</div>
+
+				<div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label" style="position:relative; top:7px;">Fecha:</label>
+					</div>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" name="fecha" value="<?php  echo date('D, d M Y H:i:s')?>">
+					</div>
+				</div>
+            </div> 
+			</div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><span class="fa fa-close"></span> Cancelar</button>
+                <button type="submit" name="add" class="btn btn-primary"><span class="fa fa-save"></span> Guardar</a>
+			</form>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ crud_pdo/login/static/php/login.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+require "conexion.php";
+$usuario=$_POST['username'];
+$password=$_POST['password'];
+
+
+    $sql = "SELECT `idUser`, `nombreUser`, `passwordUser` FROM `users` WHERE `nombreUser` = :user";
+   
+	$records = $conn->prepare("SELECT `idUser`, `nombreUser`, `passwordUser` FROM `users` WHERE `nombreUser` = :user");
+	$records->bindParam(':user', $usuario);
+
+	$records->execute();
+
+	$results = ($records->fetch(PDO::FETCH_ASSOC));
+	print_r($results);
+
+	if ($results!=null) {
+		echo '<script>location.href ="../../../index.php";</script>';
+	}else{
+		echo '<script>alert("El ususario no existe")<script>';
+		echo '<script>location.href ="../../index.html";</script>';
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
